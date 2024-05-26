a20742f89569619df37152f2fac53ff865be336e
hhoussem <houssemeddine.hamrouni@gmail.com>
2024-05-26 13:18:30 +0200

    initial commit

---
+++ src/test/java/com/example/springbootangularapp/SpringbootAngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootangularapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
