b1cd3500721b51b349dbc52692da7c2138602bd3
hhoussem <houssemeddine.hamrouni@gmail.com>
2024-05-27 00:57:35 +0200

    inject and use profile component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ProfileComponent} from "./components/profile/profile.component";
+import {UserProfile} from "./model/user-profile";
+import {UserService} from "./services/user.service";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'frontend';
+
+  public userProfile?: UserProfile;
+  constructor(private userService: UserService) {
+    userService.getUserProfile().subscribe((userProfile: UserProfile) => this.userProfile = userProfile);
+  }
 }
